cef382e8548686eba27c2eeb5149e4c14049b2d4
ASSEENONSCREEN\lindsay.strydom <lindsay.strydom>
2017-02-08 12:28:00 +0000

    adding Day 3

---
+++ HackerRankDayThree/HackerRankDayThree/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankDayThree
+{
+    class Program
+    {
+       
+
+        public static bool IsEven(int value)
+        {
+            return value % 2 == 0;
+        }
+
+        static void Main(string[] args)
+        {
+
+            int n = Int32.Parse(Console.ReadLine());
+
+            if (n >= 1 && n <= 100)
+            {
+
+                
+
+                if (IsEven(n) && n >= 2 && n <= 5 || IsEven(n) && n > 20)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+                else
+                {
+                    Console.WriteLine("Weird");
+                }
+
+                
+            }
+
+        }
+    }
+}
